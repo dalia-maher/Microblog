90132e60961a9257f5768d55e9cd154171b4d85c
heba <hebamotawei@gmail.com>
2019-04-26 15:49:16 +0300

    database setup

---
+++ app/models.py
@@ -0,0 +1,45 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db, login
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    bio = db.Column(db.String(64))
+    image = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def serialize(self):
+
+        return {
+            'username': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'id': self.id,
+            'email': self.email,
+            'bio': self.bio,
+            'image': self.image,
+        }
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
+class Post(db.Model):
